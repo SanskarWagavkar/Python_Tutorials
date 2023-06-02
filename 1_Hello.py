57cf071861e3644530f8680e681c0811e0d1033e
SanskarWagavkar <80676415+SanskarWagavkar@users.noreply.github.com>
2023-06-02 17:53:18 +0530

    Add files via upload

---
+++ 1_Hello.py
@@ -0,0 +1,5 @@
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+import imutils
+import easyocr
---
+++ 2_Data_Types.py
@@ -0,0 +1,51 @@
+# 1. String
+#a =          "hello world"
+# variable    # value
+#b = 'hello world 2'
+#print(a)
+#print(b)
+#print(type(a))
+
+# 2. Number
+
+#integer = 123
+#pi = 3.14
+
+#print(integer)
+#print(pi)
+#print(type(integer))
+#print(type(pi))
+
+# 3. List
+#a = [123, 3.14, "sanskar"]
+    #0    #1     #2
+#print(a)
+#print(type(a))
+#a[0] = 1
+#print(a)
+
+
+# 4. Tuples
+#a = (123, 3.14, "sanskar")
+#print(a)
+#print(type(a))
+
+#a[0] = 1
+#print(a)
+
+# 5. Sets
+
+#a = {1,1, 2, 2, "sanskar"}
+#print(a)
+#print(type(a))
+
+# 6. Dictionary
+#a = {"cat":"is is animal",
+#    "computer":"is a machine",
+#     2:"is s student"}
+
+#print(a["cat"])
+#print(type(a))
+
+
+
